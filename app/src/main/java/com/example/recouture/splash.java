5d37f51935a7e18a0e56e68cc3dadce4701f4f8f
Matthea Loo <>
2018-06-23 19:54:15 +0800

    i think i settle the errors alr wewww

--- app/src/main/java/com/example/recouture/splash.java
+++ app/src/main/java/com/example/recouture/splash.java
@@ -3,11 +3,11 @@ package com.example.recouture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+public class splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash);
     }
 }
