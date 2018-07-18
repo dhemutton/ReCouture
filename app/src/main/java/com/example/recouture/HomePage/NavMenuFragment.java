751641c24016915c6b61af23c60ed766179d5877
Matthea Loo <>
2018-07-19 00:30:57 +0800

    edit signout

--- app/src/main/java/com/example/recouture/HomePage/NavMenuFragment.java
+++ app/src/main/java/com/example/recouture/HomePage/NavMenuFragment.java
@@ -1,5 +1,7 @@
 package com.example.recouture.HomePage;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -35,13 +37,50 @@ import java.net.Inet4Address;
 public class NavMenuFragment extends Fragment {
 
     private static final String TAG = "NavMenuFragment";
+    private boolean isOpen = true;
+    private FragmentManager manager;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_homepagenav, container, false);
+        Button closeNav = (Button) view.findViewById(R.id.navmenuicon);
+/*
+        if (isOpen == true) {
+            closeNav.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick: navigating back to homepage");
+                    getActivity().getFragmentManager().beginTransaction().remove(NavMenuFragment.this).commit();
+
+                }
+            });
+
+        }
+*/
 
 
+        //SIGN OUT
+        //SIGN OUT
+        Button signOut = (Button) view.findViewById(R.id.button4);
+        manager = getFragmentManager();
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating back to 'signout fragment'");
+                addFragmentViewSignout();
+            }
+        });
         return view;
     }
+
+
+    private void addFragmentViewSignout() {
+        SignOutFragment fragment = new SignOutFragment();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.add(R.id.signoutfragment,fragment);
+        fragmentTransaction.commit();
+    }
 }
+
