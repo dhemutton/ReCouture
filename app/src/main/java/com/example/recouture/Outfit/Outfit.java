60d4f5c6bb0f354b44c23c63b007c0589d43ef2d
Matthea Loo <>
2018-07-21 22:44:24 +0800

    paiseh

---
+++ app/src/main/java/com/example/recouture/Outfit/Outfit.java
@@ -0,0 +1,19 @@
+package com.example.recouture.Outfit;
+
+import com.example.recouture.Item;
+
+
+public class Outfit extends Item {
+
+
+
+    public Outfit() {}
+
+
+    public Outfit(String mName, String mColor, String mImageUrl) {
+        super(mName,mColor,mImageUrl);
+    }
+
+
+
+}
