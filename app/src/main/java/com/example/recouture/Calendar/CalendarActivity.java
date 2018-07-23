f5e455d608871f26d9d522c4e87d2898319fe3f3
Matthea Loo <>
2018-07-23 16:43:34 +0800

    calendar partially done

--- app/src/main/java/com/example/recouture/Calendar/CalendarActivity.java
+++ app/src/main/java/com/example/recouture/Calendar/CalendarActivity.java
@@ -1,12 +1,18 @@
 package com.example.recouture.Calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
+import com.example.recouture.Outfit.PlanOutfit;
 import com.example.recouture.R;
 import com.example.recouture.utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -14,13 +20,53 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 public class CalendarActivity extends AppCompatActivity {
     private static final String TAG = "CalendarActivity";
     private static final int ACTIVITY_NUM = 1;
+    private CalendarView mCalendarView;
+    private TextView viewplanned;
+    private TextView newplan;
 
+    private String date;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_calendar);
         Log.d(TAG, "onCreate: started");
         setupBottomNavigationView();
+
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        viewplanned = (TextView) findViewById(R.id.viewoutfittext);
+        newplan = (TextView) findViewById(R.id.plan);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date = (dayOfMonth) + "/" + (month+1) + "/" + year;
+                Log.d(TAG, "onSelectedDayChange: dd/mm/yyyy : " + date);
+            }
+        });
+
+
+        viewplanned.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalendarActivity.this, ViewPlanned.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        });
+
+
+        newplan.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(CalendarActivity.this, PlanOutfit.class);
+                    intent.putExtra("date", date);
+                    startActivity(intent);
+                }
+
+        });
+
+
+
     }
 
     private void setupBottomNavigationView(){
---
+++ app/src/main/java/com/example/recouture/Outfit/PlanOutfit.java
@@ -0,0 +1,30 @@
+package com.example.recouture.Outfit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import com.example.recouture.R;
+
+public class PlanOutfit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewoufits);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), Click_Outfit.class);
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
